b32b625fd55b1c41fb78f360835eb1a5096855a7
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-05-02 23:55:40 -0500

    added routes and english tag

---
+++ TareaAngular2/angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['es', 'en']);
+    translate.setDefaultLang('es');
+    translate.use('es');
+    const browserLang = 'es';
+    translate.use(browserLang.match(/es|en/) ? browserLang : 'es');
+  }
+
+}
+
+
