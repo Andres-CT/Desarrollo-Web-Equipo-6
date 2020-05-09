a5827c16fae509a54ce334233b1c486ff3e7af4b
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-05-09 12:24:20 -0500

    Do not touch eol

---
+++ Evaluación Segundo Parcial/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+
+})
+
+export class AppComponent {
+  title = 'angular';
+  state: string = '';
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['es', 'en']);
+    translate.setDefaultLang('es');
+    translate.use('es');
+    const browserLang = 'es';
+    translate.use(browserLang.match(/es🇲🇽|en🇺🇸/) ? browserLang : 'es');
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
+  }
+
+}
+
+
